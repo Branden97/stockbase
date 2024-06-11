5de246df11e599b4a15d2df41cb8fdfc6196f00e
Branden Williams <1894876+Branden97@users.noreply.github.com>
2024-06-11 14:12:16 -0400

    fix(api): fix typing issues in error-handler.ts

--- apps/api/src/error-handler.ts
+++ apps/api/src/error-handler.ts
@@ -1,18 +1,16 @@
 import { error } from '@repo/logger'
-import { ErrorRequestHandler } from 'express'
+import { type ErrorRequestHandler } from 'express'
 
 export const errorHandler: ErrorRequestHandler = (err: Error, req, res, next) => {
   // TODO: Handle various error cases here
   error(err)
 
   // Set the response status code
-  // @ts-ignore
-  res.status(err.status || 500)
+  res.status('status' in err && typeof err.status === 'number' ? err.status : 500)
 
   // Send the error message as the response
   res.json({
-    // @ts-ignore
-    errors: err?.errors,
+    errors: 'errors' in err ? err.errors : undefined,
     error: err.message || 'Internal Server Error',
   })
 }
