eb6344135145dc236d1cda825d3975430742c266
Branden Williams <1894876+Branden97@users.noreply.github.com>
2024-06-12 09:45:28 -0400

    feat(api): implement listWatchlists endpoint

--- apps/api/src/operation-handlers/watchlists/listWatchlists.ts
+++ apps/api/src/operation-handlers/watchlists/listWatchlists.ts
@@ -1,13 +1,25 @@
-import { RequestHandler } from 'express'
+import { Watchlist } from '@repo/db'
+import type { RequestHandler } from 'express'
+import { asyncHandler } from '../../utils/async-handler'
+import { getPaginationMeta } from '../../utils/pagination-utils'
 
-export const listWatchlistsHandler: RequestHandler = (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: 'Tech Stocks',
-      userId: 1,
-      createdAt: '2023-01-01T00:00:00Z',
-      updatedAt: '2023-01-01T00:00:00Z',
-    },
-  ])
-}
+export const listWatchlistsHandler: RequestHandler = asyncHandler(async (req, res, next) => {
+  const limit = req.query.limit as number
+  const userId = req.userId as number | undefined
+  if (userId === undefined) {
+    res.status(401).json({ message: 'Unauthorized' })
+    return
+  }
+
+  try {
+    const results = await Watchlist.findAndCountAll({ limit, offset: req.skip, where: { userId } })
+    const itemCount = results.count
+
+    res.json({
+      watchlists: results.rows.map((watchlist) => watchlist.toJSON()),
+      paginationMeta: getPaginationMeta(req, itemCount),
+    })
+  } catch (err) {
+    next(err)
+  }
+})
