f06e17619414821a868874148e452c8fa4079619
Branden Williams <1894876+Branden97@users.noreply.github.com>
2024-06-11 21:05:53 -0400

    feat(api): implement logout endpoint

--- apps/api/src/operation-handlers/auth/logout.ts
+++ apps/api/src/operation-handlers/auth/logout.ts
@@ -1,5 +1,28 @@
-import { RequestHandler } from 'express'
+import type { RequestHandler, Request } from 'express'
+import { error } from '@repo/logger'
+import { asyncHandler } from '../../utils/async-handler'
 
-export const logoutHandler: RequestHandler = (req, res) => {
-  res.status(204).json()
-}
+/**
+ * Logout handler for the API.
+ * 
+ * @remarks
+ * This handler is responsible for logging out a user by blacklisting a JWT token pair's family
+ * and clearing the token cookies.
+ * 
+ * @param req - The Express request object.
+ * @param res - The Express response object.
+ * @returns A Promise that resolves to void.
+ */
+export const logoutHandler: RequestHandler = asyncHandler(async (req: Request, res) => { 
+  const fam = req.tokenPayload?.fam || req.refreshTokenPayload?.fam
+  if (!fam) {
+    error(`Users shouldn't be able to reach this endpoint without a token or refreshToken!`)
+    res.status(401).json({ message: 'Unauthorized' })
+    return
+  }
+
+  await req.app.jwtService.blacklistFamily(fam)
+  res.clearCookie('token')
+  res.clearCookie('refreshToken')
+  res.status(200).send('Logged out successfully')
+})
